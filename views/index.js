5466e0030a37ed5655e9a8eceff7eef4e99523e1
SolteckQuest <101373540+SolteckQuest@users.noreply.github.com>
2022-04-19 18:03:12 -0700

    Add files via upload

--- views/index.js
+++ views/index.js
@@ -1,31 +1,67 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-
-const btnSignIn = document.querySelector(".sign-in-btn"),
-    btnSignUp = document.querySelector(".sign-up-btn"),
-    signUp = document.querySelector(".sign-up"),
+const btnSignIn = document.querySelector(".ask-sumit-btn"),
+    btnCancel = document.querySelector(".cancelar-btn"),
+    btnSave = document.querySelector(".save-btn"),
+    btnCancel1 = document.querySelector(".cancelar-btn1"),
     signIn = document.querySelector(".sign-in"),
-    btnForgotPass = document.querySelector(".forgot-password-btn"),
-    btnSignIn2 = document.querySelector(".sign-in-btn-2"),
-    forgotPass = document.querySelector(".forgot-password");
+    askIn = document.querySelector(".create-quest"),
+    SaveIn = document.querySelector(".save"),
+    btnIn = document.querySelector(".btn");
 
-
-navToggle.addEventListener("click", () =>{
+navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible");
 });
-
-document.addEventListener("click", e =>{
-    if(e.target === btnSignIn || e.target === btnSignUp){
+//Botones
+document.addEventListener("click", e => {
+    if (e.target === btnSignIn || e.target === btnCancel) {
         signIn.classList.toggle("active");
-        signUp.classList.toggle("active");
+        askIn.classList.toggle("active");
+        btnIn.classList.toggle("active");
     }
 });
 
-document.addEventListener("click", e =>{
-    if(e.target === btnForgotPass || e.target === btnSignIn2){
-        signIn.classList.toggle("active");
-        forgotPass.classList.toggle("active");
+document.addEventListener("click", e => {
+    if (e.target === btnSave || e.target === btnCancel1) {
+        SaveIn.classList.toggle("active");
     }
 });
+
+//Range input
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+const numberInput = document.querySelector('input[type="number"]')
+
+function handleInputChange(e) {
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    }
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+})
+
+numberInput.addEventListener('input', handleInputChange)
+
+
+
+const renderLogin = () => {
+    const loginForm = document.getElementById('login-form')
+    loginForm.onsubmit = (e) => {
+        e.preventDefault()
+        window.location.replace('sesion.html')
+    }
+}
+
+window.onload = () => {
+    renderLogin()
+}
+
 
 
--- views/sesion.js
+++ views/sesion.js
@@ -1,56 +1,11 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-const btnQuest = document.querySelector(".quest-btn"),
-    CreateQuest = document.querySelector(".create-quest"),
-    Option1 = document.querySelector(".option"),
-    MyQuest = document.querySelector(".my-quest"),
-    btnMyQuest = document.querySelector(".my-quest-btn"),
-    MyQuestOption = document.querySelector(".my-quest-option"),
-    MyQuest2 = document.querySelector(".my-quest-2"),
-    MyQuestHistorial = document.querySelector(".my-quest-historial"),
-    btnMyQuestHistorial = document.querySelector(".historial-btn");
+const btnCancelar = document.querySelector(".cancelar-btn");
 
-navToggle.addEventListener("click", () =>{
+
+navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible");
 });
-
-document.addEventListener("click", e =>{
-    if(e.target === btnQuest /*|| e.target === btnSignUp*/){
-        CreateQuest.classList.toggle("active");
-        Option1.classList.toggle("active");
-    }
-    if(e.target === btnMyQuest /*|| e.target === btnSignUp*/){
-        MyQuest.classList.toggle("active");
-        Option1.classList.toggle("active");
-        MyQuestOption.classList.toggle("active");
-    }
-    if(e.target === btnMyQuestHistorial /*|| e.target === btnSignUp*/){
-        MyQuest2.classList.toggle("active");
-        Option1.classList.toggle("active");
-        MyQuestHistorial.classList.toggle("active");
-    }
-});
-//Range input
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-const numberInput = document.querySelector('input[type="number"]')
-
-function handleInputChange(e) {
-  let target = e.target
-  if (e.target.type !== 'range') {
-    target = document.getElementById('range')
-  } 
-  const min = target.min
-  const max = target.max
-  const val = target.value
-  
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-}
-
-rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
-})
-
-numberInput.addEventListener('input', handleInputChange)
 
 //fecha
 date = new Date();
@@ -59,3 +14,4 @@ month = date.getMonth() + 1;
 day = date.getDate();
 document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year;
 document.getElementById("current_date-2").innerHTML = day + "/" + month + "/" + year;
+
